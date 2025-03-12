fae4162a5c794c70eb1f4a8de204786c6c7825e4
Silence0926 <subscriberwongzijin@ipk.edu.my>
2025-03-12 09:02:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     buttons.forEach(button => {
         button.addEventListener("click", function () {
             const productElement = button.parentElement;
-            const productName = productElement.querySelector(".product-name").innerText;
-            const productPrice = parseFloat(productElement.querySelector(".product-price").innerText.replace("RM", ""));
+            const productName = productElement.querySelector("p:nth-of-type(1)").innerText;  // 第一个 <p> 是商品名称
+            const productPrice = parseFloat(productElement.querySelector("p:nth-of-type(2)").innerText.replace("RM", "")); // 第二个 <p> 是价格
             const productImage = productElement.querySelector("img").src;
             
             addToCart(productName, productPrice, productImage);
