fbf7ec932c5ee80ddaee102b7e6c9d9cb7a38020
Silence0926 <subscriberwongzijin@ipk.edu.my>
2025-03-12 12:24:07 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,47 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 600;
+
+const plane = {
+    x: canvas.width / 2 - 25,
+    y: canvas.height - 100,
+    width: 50,
+    height: 50,
+    speed: 5
+};
+
+const keys = {};
+
+document.addEventListener("keydown", (event) => {
+    keys[event.key] = true;
+});
+
+document.addEventListener("keyup", (event) => {
+    keys[event.key] = false;
+});
+
+function update() {
+    if (keys["ArrowLeft"] && plane.x > 0) {
+        plane.x -= plane.speed;
+    }
+    if (keys["ArrowRight"] && plane.x < canvas.width - plane.width) {
+        plane.x += plane.speed;
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(plane.x, plane.y, plane.width, plane.height);
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
